bd740e9e18106ad63970ec49e03efe9d36a96b85
Remi <remi@sidefx.com>
2023-03-29 16:18:21 -0400

    Move the order of import to make the wsgi script work with old Django and newer version of Gunicorn.

--- wsgi-script/wsgi.py
+++ wsgi-script/wsgi.py
@@ -1,14 +1,13 @@
 import os
 import sys
-import django.conf
-from django.core.wsgi import get_wsgi_application
-
 sys.path.append('/var/www')
 sys.path.append('/var/www/stats')
 sys.path.append('/var/www/stats/stats_core')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stats_core.settings")
 
-# See http://stackoverflow.com/questions/11383176/problems-hosting-multiple-django-sites-settings-cross-over
-django.conf.ENVIRONMENT_VARIABLE = "DJANGO_STATS_SETTINGS_MODULE"
+from django.core.wsgi import get_wsgi_application
+
+
 os.environ['DJANGO_STATS_SETTINGS_MODULE'] = 'stats_core.settings'
 
 application = get_wsgi_application()
